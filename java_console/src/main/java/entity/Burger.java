0a63f87b060c116a5e016dfe1e609b3cf740c23f
daniel-sysnet <danielmolombo96@gmail.com>
2025-12-14 19:27:21 +0000

    Ajout des entités Burger, Complement et Menu

---
+++ java_console/src/main/java/entity/Burger.java
@@ -0,0 +1,71 @@
+package entity;
+
+public class Burger {
+    private int id;
+    private String nom;
+    private double prix;
+    private String image;
+    private String description;
+
+    public Burger() {
+    }
+
+    public Burger(int id, String nom, double prix, String image, String description) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+        this.image = image;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Burger{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prix=" + prix +
+                ", image='" + image + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
