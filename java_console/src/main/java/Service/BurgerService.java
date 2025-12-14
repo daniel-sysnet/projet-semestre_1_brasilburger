8c2bc2457aeae479325b7027c6922c7cafe37d30
daniel-sysnet <danielmolombo96@gmail.com>
2025-12-14 19:44:54 +0000

    Ajout de la methode de listage des burgers dans BurgerService

--- java_console/src/main/java/Service/BurgerService.java
+++ java_console/src/main/java/Service/BurgerService.java
@@ -22,4 +22,8 @@ public class BurgerService {
     public void archiverBurger(int id) {
         burgerRepository.archive(id);
     }
+
+    public List<Burger> listerBurgers() {
+        return burgerRepository.findAll();
+    }
 }
