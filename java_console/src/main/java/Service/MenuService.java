072a772fe07897fc3907b84edc0bdd8665b6077d
daniel-sysnet <danielmolombo96@gmail.com>
2025-12-14 19:56:28 +0000

    Ajout de la methode d ajout d un menu dans MenuService

---
+++ java_console/src/main/java/Service/MenuService.java
@@ -0,0 +1,19 @@
+package service;
+
+import entity.Menu;
+import repository.MenuRepository;
+import java.util.List;
+
+public class MenuService {
+    private MenuRepository menuRepository;
+
+    public MenuService() {
+        this.menuRepository = new MenuRepository();
+    }
+
+    public void ajouterMenu(Menu menu) {
+        menuRepository.save(menu);
+    }
+
+
+}
