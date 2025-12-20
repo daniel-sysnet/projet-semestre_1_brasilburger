62b7a7a2f59af6232dfd3cf0f03f7ac264c52013
daniel-sysnet <danielmolombo96@gmail.com>
2025-12-20 23:46:20 +0000

    fix: Déplacer Dockerfile dans csharp_web et ajuster les chemins pour Render

--- csharp_web/obj/Debug/net10.0/csharp_web.AssemblyInfo.cs
+++ csharp_web/obj/Debug/net10.0/csharp_web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764f22efc920b58974bc55cacc20fac62529afde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfffefd1147e4b5136ca3c42a4fa63948c51c797")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
