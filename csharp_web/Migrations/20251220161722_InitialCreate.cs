ed2b1b8766192a2ad1173873ba87861d1c503fed
daniel-sysnet <danielmolombo96@gmail.com>
2025-12-20 16:27:06 +0000

    Configuration de la base de données PostgreSQL et DbContext EF Core

---
+++ csharp_web/Migrations/20251220161722_InitialCreate.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace csharp_web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Burgers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Prix = table.Column<decimal>(type: "numeric", nullable: false),
+                    Image = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Burgers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Prenom = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Telephone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Complements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Prix = table.Column<decimal>(type: "numeric", nullable: false),
+                    Image = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Complements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gestionnaires",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Prenom = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Telephone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Login = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Password = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gestionnaires", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Prix = table.Column<decimal>(type: "numeric", nullable: false),
+                    Image = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Paiements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Montant = table.Column<decimal>(type: "numeric", nullable: false),
+                    Methode = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paiements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Zones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Prix = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Zones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Livreurs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Prenom = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Telephone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    ZoneId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livreurs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Livreurs_Zones_ZoneId",
+                        column: x => x.ZoneId,
+                        principalTable: "Zones",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Commandes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ClientId = table.Column<int>(type: "integer", nullable: true),
+                    Etat = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    ZoneId = table.Column<int>(type: "integer", nullable: true),
+                    LivreurId = table.Column<int>(type: "integer", nullable: true),
+                    PaiementId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commandes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Commandes_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Commandes_Livreurs_LivreurId",
+                        column: x => x.LivreurId,
+                        principalTable: "Livreurs",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Commandes_Paiements_PaiementId",
+                        column: x => x.PaiementId,
+                        principalTable: "Paiements",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Commandes_Zones_ZoneId",
+                        column: x => x.ZoneId,
+                        principalTable: "Zones",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LigneCommandes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CommandeId = table.Column<int>(type: "integer", nullable: false),
+                    BurgerId = table.Column<int>(type: "integer", nullable: true),
+                    MenuId = table.Column<int>(type: "integer", nullable: true),
+                    ComplementId = table.Column<int>(type: "integer", nullable: true),
+                    Quantite = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LigneCommandes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LigneCommandes_Burgers_BurgerId",
+                        column: x => x.BurgerId,
+                        principalTable: "Burgers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_LigneCommandes_Commandes_CommandeId",
+                        column: x => x.CommandeId,
+                        principalTable: "Commandes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LigneCommandes_Complements_ComplementId",
+                        column: x => x.ComplementId,
+                        principalTable: "Complements",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_LigneCommandes_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "Menus",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commandes_ClientId",
+                table: "Commandes",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commandes_LivreurId",
+                table: "Commandes",
+                column: "LivreurId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commandes_PaiementId",
+                table: "Commandes",
+                column: "PaiementId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commandes_ZoneId",
+                table: "Commandes",
+                column: "ZoneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LigneCommandes_BurgerId",
+                table: "LigneCommandes",
+                column: "BurgerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LigneCommandes_CommandeId",
+                table: "LigneCommandes",
+                column: "CommandeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LigneCommandes_ComplementId",
+                table: "LigneCommandes",
+                column: "ComplementId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LigneCommandes_MenuId",
+                table: "LigneCommandes",
+                column: "MenuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livreurs_ZoneId",
+                table: "Livreurs",
+                column: "ZoneId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Gestionnaires");
+
+            migrationBuilder.DropTable(
+                name: "LigneCommandes");
+
+            migrationBuilder.DropTable(
+                name: "Burgers");
+
+            migrationBuilder.DropTable(
+                name: "Commandes");
+
+            migrationBuilder.DropTable(
+                name: "Complements");
+
+            migrationBuilder.DropTable(
+                name: "Menus");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Livreurs");
+
+            migrationBuilder.DropTable(
+                name: "Paiements");
+
+            migrationBuilder.DropTable(
+                name: "Zones");
+        }
+    }
+}
