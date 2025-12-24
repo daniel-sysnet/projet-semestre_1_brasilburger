1497999936eb7ca886b219908d9550c8648de808
daniel-sysnet <danielmolombo96@gmail.com>
2025-12-24 23:04:39 +0000

    feat: créer le dashboard gestionnaire avec statistiques dynamiques et design

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CommandeRepository;
 
 class DashboardController extends AbstractController
 {
+    public function __construct(private CommandeRepository $commandeRepository) {}
+
     #[Route('/dashboard', name: 'dashboard')]
     public function index(): Response
     {
+        $stats = [
+            'commandes_en_cours' => $this->commandeRepository->countByEtatDuJour('En cours'),
+            'commandes_validees' => $this->commandeRepository->countByEtatDuJour('Validee'),
+            'recettes_journalieres' => $this->commandeRepository->getChiffreAffairesDuJour(),
+            'commandes_annulees' => $this->commandeRepository->countByEtatDuJour('Annulee'),
+        ];
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'stats' => $stats,
         ]);
     }
 }
